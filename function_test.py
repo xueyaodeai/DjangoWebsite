daf9555239be8c101a49f295f23893ca156dd719
XueYao <xueyaodeai@gmail.com>
2016-12-28 23:08:34 +0800

    add comment to function_test.py && update .gitignore

--- function_test.py
+++ function_test.py
@@ -11,10 +11,14 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        # open the index page
         self.browser.get('http://localhost:8000')
 
+        # the title and head of the page include To-Do
         self.assertIn('To-Do', self.browser.title)
         self.fail('Finish the test')
 
+        # the page invite her to type a To-Do
+
 if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+    unittest.main()
