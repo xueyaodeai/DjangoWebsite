8a7674ed6ea7e0fe821338fcc400cbb23b043e31
XueYao <xueyaodeai@gmail.com>
2017-01-18 00:08:15 +0800

    Functional test now checks we can input a to-do item

--- function_test.py
+++ function_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -12,14 +13,42 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # open the index page
+        # 伊迪丝听说有一个很酷的在线待办事项应用
+        # 她去看了这个应用的首页
         self.browser.get('http://localhost:8000')
 
-        # the title and head of the page include To-Do
+        # 她注意到网页的标题和头部都包含"To-Do"这个词
         self.assertIn('To-Do', self.browser.title)
-        # self.fail('Finish the test')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-        # the page invite her to type a To-Do
+        # 应用邀请她输入一个待办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            "Enter a to-do item"
+        )
+
+        # 她在一个文本框中输入了"Buy peacock feathers"(购买孔雀羽毛)
+        # 伊迪丝的爱好是使用假蝇做鱼饵钓鱼
+        inputbox.send_keys('Buy peacock feathers')
+
+        # 她按回车键后，页面更新了
+        # 待办事项列表中显示了"1: But peacock feathers"
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Bug peacock feathers' for row in rows)
+        )
+
+        # 页面又显示了一个文本框，可以输入其他的待办事项
+        # 她输入了"Use peacock feathers to make a fly"(使用孔雀羽毛做假蝇)
+        # 伊迪丝做事很有条理
+        self.fail('Finish the test')
+
+        # 页面再次更新，她的清单中显示了这两个待办事项
 
 if __name__ == '__main__':
     unittest.main()
