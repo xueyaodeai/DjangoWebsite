ce7cac288258a9782706d44ff4728b94d77f4191
XueYao <xueyaodeai@gmail.com>
2017-01-18 00:07:21 +0800

    update lists/tests.py

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 
-from lists.views import home_page
+from .views import home_page
 
 
 class HomePageTest(TestCase):
